e089995f2836b4567854ab51b3b5747b4e80c6b2
Azecko <samikarin24@gmail.com>
2024-08-08 10:48:24 +0200

    [feature] Better looking SignIn button and SignOut button
    Using `Button` component from `@headlessui/react`
    
    Also added `@heroicons/react` for later.

--- src/app/components/auth/SignOutButton.tsx
+++ src/app/components/auth/SignOutButton.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import { Button } from "@headlessui/react";
 import { signOut } from "next-auth/react"
  
 export function SignOutButton() {
-  return <button onClick={() => signOut()}>Sign Out</button>
+  return (
+    <Button
+      className="rounded-xl text-sm font-semibold bg-sky-500 py-2 px-4 text-sm text-white transition ease-in-out data-[hover]:bg-sky-600"
+      onClick={() => signOut()}>
+        Sign Out
+    </Button>
+  )
 }
