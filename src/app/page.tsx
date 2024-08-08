91cd81399ebb20ec61c9479056babad88c9db6da
Azecko <samikarin24@gmail.com>
2024-08-08 11:49:18 +0200

    [feature] Header
    Quick header, template from [tailwindui](https://tailwindui.com/components/marketing/elements/headers) and redesigned.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "../../auth";
 import { SignInButton } from "./components/auth/SignInButton";
 import { SignOutButton } from "./components/auth/SignOutButton";
+import Header from "./components/Header";
 import UserAvatar from "./components/UserAvatar";
 
 export default async function Home() {
@@ -9,6 +10,7 @@ export default async function Home() {
 
   return (
     <div>
+      <Header session={session} />
       <div className="flex min-h-screen flex-col items-center bg-gray-900">
         <h1>Welcome to Nextra !</h1>
         {session ? (
